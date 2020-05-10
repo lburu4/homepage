c723dd320b5eb799dbddcc80513070f86ded5f31
Lukasz Burnat <lburu4@gmail.com>
2020-05-10 15:48:37 +0100

    all change in index

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const firstname = 'Pawel';
 const age = 42;
 
-console.log(firstname);
+console.log(firstname); 
 console.log(age);
 console.log(`Siema ${firstname}, ktory ma ${age} lata`);
 console.log('witam');
